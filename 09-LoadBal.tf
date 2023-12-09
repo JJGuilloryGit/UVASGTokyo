0e0df2f4e249c4670ff96fff20ffa41270b1d3b9
JJGuilloryGit <116040184+JJGuilloryGit@users.noreply.github.com>
2023-12-08 18:18:36 -0600

    Add files via upload

--- 09-LoadBal.tf
+++ 09-LoadBal.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "app1_alb" {
   name               = "App1-load-balancer"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.App1_SG2_LB1.id]
+  security_groups    = [aws_security_group.App1_443_SG2_LB1.id]
   subnets            = [
     aws_subnet.public-ap-northeast-1a.id,
     aws_subnet.public-ap-northeast-1c.id,
@@ -23,6 +23,27 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app1_alb.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app1_tg.arn
+  }
+}
+
+data "aws_acm_certificate" "cert" {
+  domain   = "theuntouchablevisionaires.com"
+  statuses = ["ISSUED"]
+  most_recent = true
+}
+
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app1_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"  # or whichever policy suits your requirements
+  certificate_arn   = data.aws_acm_certificate.cert.arn
+
 
   default_action {
     type             = "forward"
